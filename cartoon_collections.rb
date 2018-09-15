e5ede7cf3108ac77eabf78311e029c219b114a81
Christina Delavar <c.delavar@gmail.com>
2018-09-15 16:55:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,10 +3,10 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  new_array = array.collect {|word| word.capitalize.insert(-1, "!")}
+  array.collect {|word| word.capitalize.insert(-1, "!")}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 
